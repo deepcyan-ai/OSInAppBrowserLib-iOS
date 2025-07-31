6e0f88590b36c5a8bfa0597b0646c8fcde5b3562
Sayandeep Purkayasth <sayandeep@Sayandeeps-iMac-Pro.local>
2025-07-31 16:37:24 +0530

    somewhat working

--- OSInAppBrowserLib/WebView/OSIABWebViewModel.swift
+++ OSInAppBrowserLib/WebView/OSIABWebViewModel.swift
@@ -158,13 +158,24 @@ extension OSIABWebViewModel: WKNavigationDelegate {
         var shouldStart = true
         
         guard let url = navigationAction.request.url, url == navigationAction.request.mainDocumentURL else { return decisionHandler(.cancel) }
+        print("decidePolicyFor \(url)")
         
         // if is an app store, tel, sms, mailto or geo link, let the system handle it, otherwise it fails to load it
         if ["itms-appss", "itms-apps", "tel", "sms", "mailto", "geo", "upi", "tez", "gpay", "googlepay", "paytm", "paytmmp", "phonepe", "ppe", "cred"].contains(url.scheme) {
-            webView.stopLoading()
-            self.callbackHandler.onDelegateURL(url)
-            shouldStart = false
-        }
+            print("url scheme matched \(url)")
+            let canOpen = UIApplication.shared.canOpenURL(url)
+            print("url scheme canOpen \(canOpen)")
+	    if (canOpen) {
+		    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+		    print("url scheme open canOpen)")
+		    webView.stopLoading()
+		    self.callbackHandler.onDelegateURL(url)
+		    shouldStart = false
+	    }
+	    else {
+		    shouldStart = true
+	    }
+	}
         
         if shouldStart {
             if navigationAction.targetFrame != nil {
